977ef00ffe40e840c51aceac0a933797a41db81b
Joel <joelcollingwood93@gmail.com>
2025-03-22 10:33:09 -0700

    cleaning up form

--- server/src/main/kotlin/jc/kotlin/template/server/pages/components/FormComponents.kt
+++ server/src/main/kotlin/jc/kotlin/template/server/pages/components/FormComponents.kt
@@ -7,6 +7,9 @@ import kotlin.collections.component1
 import kotlin.collections.component2
 import kotlin.collections.set
 
+// basic styling for form label/input spacing
+val formFieldStyles = setOf("flex", "flex-col", "gap-2")
+
 fun FlowContent.formComponent() {
     div {
         classes = setOf("flex", "flex-col", "gap-4")
@@ -24,211 +27,12 @@ fun FlowContent.formComponent() {
 
             classes = setOf("flex", "flex-col", "gap-4")
 
-            val formFieldStyles = setOf("flex", "flex-col", "gap-2")
+            formInputs()
+            radioFormInputs()
+            checkboxFormInputs()
+            selectFormInputs()
+            textareaFormInputs()
 
-            // inputs
-            span {
-                classes = formFieldStyles
-                label {
-                    +"First Input"
-                }
-                input {
-                    classes = inputStyles
-
-                    type = InputType.text
-                    name = "first_input"
-
-                    placeholder = "Placeholder"
-                }
-            }
-
-            span {
-                classes = formFieldStyles
-                label {
-                    required()
-                    +"Second Input w/ Validations"
-                }
-                input {
-                    hxPost("/components/form/validate?input=second_input")
-                    hxTrigger("change")
-                    hxSwap()
-                    hxSync()
-
-                    classes = inputStyles
-
-                    type = InputType.text
-                    name = "second_input"
-                    required = true
-
-                    placeholder = "Must be between 3 and 10 characters"
-                }
-                p {
-                    id = "second_input_error"
-                }
-            }
-
-            // radio buttons
-            p {
-                required()
-                +"Pick your favorite number:"
-            }
-            div {
-                classes = setOf("flex", "flex-col", "gap-3")
-
-                val radioButtonStyles = setOf("flex", "flex-row", "gap-4")
-                val radioInputStyles = setOf("cursor-pointer")
-                val radioLabelStyles = setOf("cursor-pointer", "block")
-
-                div {
-                    classes = radioButtonStyles
-                    input {
-                        classes = radioInputStyles
-                        type = InputType.radio
-                        name = "first_radio_group"
-                        id = "first_radio_group_one"
-                        value = "one"
-                    }
-                    label {
-                        classes = radioLabelStyles
-                        attributes["for"] = "first_radio_group_one"
-                        +"One"
-                    }
-                }
-                div {
-                    classes = radioButtonStyles
-                    input {
-                        classes = radioInputStyles
-                        type = InputType.radio
-                        name = "first_radio_group"
-                        id = "first_radio_group_two"
-                        value = "two"
-                    }
-                    label {
-                        classes = radioLabelStyles
-                        attributes["for"] = "first_radio_group_two"
-                        +"Two"
-                    }
-                }
-                div {
-                    classes = radioButtonStyles
-                    input {
-                        classes = radioInputStyles
-                        type = InputType.radio
-                        name = "first_radio_group"
-                        id = "first_radio_group_three"
-                        value = "three"
-                    }
-                    label {
-                        classes = radioLabelStyles
-                        attributes["for"] = "first_radio_group_three"
-                        +"Three"
-                    }
-                }
-                p {
-                    id = "first_radio_group_error"
-                }
-            }
-
-            p {
-                required()
-                +"Fancier radio - pick your favorite color:"
-            }
-            div {
-                classes = setOf("flex", "flex-col", "gap-1")
-                div {
-                    classes = setOf("flex", "flex-row", "gap-3")
-
-                    val radioButtonStyles = setOf(
-                        "flex",
-                        "flex-row",
-                        "gap-3",
-                        "relative",
-                        "border",
-                        "border-gray-400",
-                        "hover:border-gray-500",
-                        "rounded-md"
-                    )
-                    val radioInputStyles =
-                        setOf(
-                            "peer",
-                            "cursor-pointer",
-                            "absolute",
-                            "opacity-0",
-                            "w-full",
-                            "h-full",
-                            "rounded-md"
-                        )
-                    val radioLabelStyles =
-                        setOf(
-                            "cursor-pointer",
-                            "block",
-                            "p-4",
-                            "rounded-md",
-                            "shadow-md",
-                            "peer-focus:shadow-lg",
-                        )
-
-
-                    div {
-                        classes = radioButtonStyles
-                        input {
-                            classes = radioInputStyles
-                            type = InputType.radio
-                            name = "second_radio_group"
-                            id = "second_radio_group_yellow"
-                            value = "yellow"
-                        }
-                        label {
-                            classes =
-                                radioLabelStyles + setOf("peer-checked:bg-yellow-100", "peer-checked:border-yellow-400")
-                            attributes["for"] = "second_radio_group_yellow"
-                            +"Yellow"
-                        }
-                    }
-                    div {
-                        classes = radioButtonStyles
-                        input {
-                            classes = radioInputStyles
-                            type = InputType.radio
-                            name = "second_radio_group"
-                            id = "second_radio_group_red"
-                            value = "red"
-                        }
-                        label {
-                            classes = radioLabelStyles + setOf("peer-checked:bg-red-100", "peer-checked:border-red-400")
-                            attributes["for"] = "second_radio_group_red"
-                            +"Red"
-                        }
-                    }
-                    div {
-                        classes = radioButtonStyles
-                        input {
-                            classes = radioInputStyles
-                            type = InputType.radio
-                            name = "second_radio_group"
-                            id = "second_radio_group_blue"
-                            value = "blue"
-                        }
-                        label {
-                            classes =
-                                radioLabelStyles + setOf("peer-checked:bg-blue-100", "peer-checked:border-blue-400")
-                            attributes["for"] = "second_radio_group_blue"
-                            +"Blue"
-                        }
-                    }
-                }
-                p {
-                    id = "second_radio_group_error"
-                }
-            }
-
-            // TODO checkbox
-
-            // TODO select
-
-            // TODO textarea
-
-            // TODO disable button until valid form
             span {
                 classes = setOf("mt-4")
                 jcButton(iconButtonStyles + setOf("pr-10")) {
@@ -343,4 +147,239 @@ fun FlowContent.formValidation(input: String, parameters: Parameters) {
     val errorMap = validateForm(parameters)
     // only show validation error for provided input
     showValidationErrors(mapOf(input to errorMap[input]))
+}
+
+fun FlowContent.formInputs() {
+    // inputs
+    span {
+        classes = formFieldStyles
+        label {
+            +"First Input"
+        }
+        input {
+            classes = inputStyles
+
+            type = InputType.text
+            name = "first_input"
+
+            placeholder = "Placeholder"
+        }
+    }
+
+    span {
+        classes = formFieldStyles
+        label {
+            required()
+            +"Second Input w/ Validations"
+        }
+        input {
+            hxPost("/components/form/validate?input=second_input")
+            hxTrigger("change")
+            hxSwap()
+            hxSync()
+
+            classes = inputStyles
+
+            type = InputType.text
+            name = "second_input"
+            required = true
+
+            placeholder = "Must be between 3 and 10 characters"
+        }
+        p {
+            id = "second_input_error"
+        }
+    }
+}
+
+fun FlowContent.radioFormInputs() {
+    // radio buttons
+    div {
+        classes = setOf("flex", "flex-col", "md:flex-row", "w-full", "justify-between")
+
+        div {
+            classes = formFieldStyles
+            p {
+                required()
+                +"Pick your favorite number:"
+            }
+            div {
+                classes = setOf("flex", "flex-col", "gap-3")
+
+                val radioButtonStyles = setOf("flex", "flex-row", "gap-4")
+                val radioInputStyles = setOf("cursor-pointer")
+                val radioLabelStyles = setOf("cursor-pointer", "block")
+
+                div {
+                    classes = radioButtonStyles
+                    input {
+                        classes = radioInputStyles
+                        type = InputType.radio
+                        name = "first_radio_group"
+                        id = "first_radio_group_one"
+                        value = "one"
+                    }
+                    label {
+                        classes = radioLabelStyles
+                        attributes["for"] = "first_radio_group_one"
+                        +"One"
+                    }
+                }
+                div {
+                    classes = radioButtonStyles
+                    input {
+                        classes = radioInputStyles
+                        type = InputType.radio
+                        name = "first_radio_group"
+                        id = "first_radio_group_two"
+                        value = "two"
+                    }
+                    label {
+                        classes = radioLabelStyles
+                        attributes["for"] = "first_radio_group_two"
+                        +"Two"
+                    }
+                }
+                div {
+                    classes = radioButtonStyles
+                    input {
+                        classes = radioInputStyles
+                        type = InputType.radio
+                        name = "first_radio_group"
+                        id = "first_radio_group_three"
+                        value = "three"
+                    }
+                    label {
+                        classes = radioLabelStyles
+                        attributes["for"] = "first_radio_group_three"
+                        +"Three"
+                    }
+                }
+                p {
+                    id = "first_radio_group_error"
+                }
+            }
+        }
+
+        div {
+            classes = formFieldStyles
+            p {
+                required()
+                +"Fancier radio - pick your favorite color:"
+            }
+            div {
+                classes = setOf("flex", "flex-col", "gap-1")
+                div {
+                    classes = setOf("grid", "grid-cols-3", "gap-3")
+
+                    val radioButtonStyles = setOf(
+                        "flex",
+                        "flex-row",
+                        "gap-3",
+                        "relative",
+                        "border",
+                        "w-full",
+                        "border-gray-400",
+                        "hover:border-gray-500",
+                        "rounded-md"
+                    )
+                    val radioInputStyles =
+                        setOf(
+                            "peer",
+                            "cursor-pointer",
+                            "absolute",
+                            "opacity-0",
+                            "w-full",
+                            "h-full",
+                            "rounded-md"
+                        )
+                    val radioLabelStyles =
+                        setOf(
+                            "cursor-pointer",
+                            "block",
+                            "p-4",
+                            "w-full",
+                            "text-center",
+                            "rounded-md",
+                            "shadow-md",
+                            "peer-focus:shadow-lg",
+                        )
+
+
+                    div {
+                        classes = radioButtonStyles
+                        input {
+                            classes = radioInputStyles
+                            type = InputType.radio
+                            name = "second_radio_group"
+                            id = "second_radio_group_yellow"
+                            value = "yellow"
+                        }
+                        label {
+                            classes =
+                                radioLabelStyles + setOf(
+                                    "peer-checked:bg-yellow-100",
+                                    "peer-checked:border-yellow-400"
+                                )
+                            attributes["for"] = "second_radio_group_yellow"
+                            +"Yellow"
+                        }
+                    }
+                    div {
+                        classes = radioButtonStyles
+                        input {
+                            classes = radioInputStyles
+                            type = InputType.radio
+                            name = "second_radio_group"
+                            id = "second_radio_group_red"
+                            value = "red"
+                        }
+                        label {
+                            classes =
+                                radioLabelStyles + setOf(
+                                    "peer-checked:bg-red-100",
+                                    "peer-checked:border-red-400"
+                                )
+                            attributes["for"] = "second_radio_group_red"
+                            +"Red"
+                        }
+                    }
+                    div {
+                        classes = radioButtonStyles
+                        input {
+                            classes = radioInputStyles
+                            type = InputType.radio
+                            name = "second_radio_group"
+                            id = "second_radio_group_blue"
+                            value = "blue"
+                        }
+                        label {
+                            classes =
+                                radioLabelStyles + setOf(
+                                    "peer-checked:bg-blue-100",
+                                    "peer-checked:border-blue-400"
+                                )
+                            attributes["for"] = "second_radio_group_blue"
+                            +"Blue"
+                        }
+                    }
+                }
+                p {
+                    id = "second_radio_group_error"
+                }
+            }
+        }
+    }
+}
+
+fun FlowContent.checkboxFormInputs() {
+
+}
+
+fun FlowContent.selectFormInputs() {
+
+}
+
+fun FlowContent.textareaFormInputs() {
+
 }
